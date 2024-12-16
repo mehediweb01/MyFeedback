ba21e480309bd5c23bac0cea5cb5ef86211524dc
First-project <mehedihasan0937580@gmail.com>
2024-12-16 14:42:47 +0600

    reviews section done & reviewCard design code update

--- src/components/HomePage/Reviews.jsx
+++ src/components/HomePage/Reviews.jsx
@@ -1,5 +1,59 @@
+import ReviewsCard from "./ReviewsCard";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Reviews = () => {
-  return <></>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/reviews.json")
+      .then((res) => res.json())
+      .then((jsonData) => setData(jsonData));
+  });
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="w-[90%] mx-auto py-12 md:py-24">
+        <h1 className="font-lexend font-extrabold text-xl sm:text-heading leading-10 text-EerieBlack1E mb-12">
+          Recents avtivities
+        </h1>
+
+        <div>
+          <Slider {...settings}>
+            {data.map((items, i) => (
+              <ReviewsCard key={i} items={items} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Reviews;
