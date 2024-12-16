475d86f47a8eec791f51b4a3e0c8431e9b2b8cd1
First-project <mehedihasan0937580@gmail.com>
2024-12-17 00:37:01 +0600

    magnificent section design done

--- src/components/RestaurantDetails/Index.jsx
+++ src/components/RestaurantDetails/Index.jsx
@@ -1,6 +1,7 @@
 import Footer from "../shared/Footer";
 import Breadcrumbs from "./Breadcrumb";
 import Hero from "./Hero";
+import Magnificent from "./Magnificent";
 
 const IndexRes = () => {
   return (
@@ -8,6 +9,7 @@ const IndexRes = () => {
       <div>
         <Breadcrumbs />
         <Hero />
+        <Magnificent />
       </div>
       <Footer />
     </>
